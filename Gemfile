11473dc13d95aee5c34238a83ba02f7c080e1859
Kamil Milewski <kamil.milewski@hotmail.com>
2017-01-24 15:25:19 +0100

    Implement index and new post funcionality

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'haml'
 # Simple ImageUpload gem
 gem "paperclip", "~> 5.0.0"
+# For simple forms
+gem 'simple_form'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -51,6 +53,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'rubocop'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,36 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show]
+
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
   end
 
-  def show
+  def show; end
+
+  def update; end
+
+  def delete; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
   end
 
-  def update
-  end
-
-  def delete
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
