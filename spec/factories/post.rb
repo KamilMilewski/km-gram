d2cca9d25d30e54897bac355f4a7810f6a746b99
Kamil Milewski <kamil.milewski@hotmail.com>
2017-02-01 00:02:39 +0100

    Create specs for voting

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -3,6 +3,6 @@ FactoryGirl.define do
     sequence(:caption) { |n| "kitten pic #{n}" }
     image Rack::Test::UploadedFile.new(Rails.root +
                                    'spec/files/images/kitten.png', 'image/png')
-    user_id 1
+    user nil
   end
 end
---
+++ spec/features/liking_posts_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper.rb'
+
+feature 'Liking ones posts' do
+  background do
+    @user = create(:user)
+    @post = create(:post, user_id: @user.id)
+    log_in @user
+    visit '/'
+  end
+
+  scenario 'can like a post' do
+    click_link "like-button-#{@post.id}"
+    expect(page).to have_css('a.liked-post')
+    # Expect to see my name within the "liked by" area of the post.
+    within('div.liked-by') do
+      expect(page).to have_content(@user.name)
+    end
+  end
+
+  scenario 'can unlike a post' do
+    click_link "like-button-#{@post.id}"
+    expect(page).to have_css('a.liked-post')
+    # Expect to see my name within the "liked by" area of the post.
+    within('div.liked-by') do
+      expect(page).to have_content(@user.name)
+    end
+    # Click the "like" button once more.
+    click_link "like-button-#{@post.id}"
+    # Expect to see the "unlikes-post" class on the button.
+    expect(page).to have_css('a.unlikes-post')
+    # Expect to NOT see my name within the "liked by" area of the post
+    within('div.liked-by') do
+      expect(page).to_not have_content(@user.name)
+    end
+  end
+end
