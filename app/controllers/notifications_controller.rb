62bc5ec639c32bee3d24647e041f6aeeccc2bb31
Kamil Milewski <kamil.milewski@hotmail.com>
2017-03-18 16:16:17 +0100

    Implement notifications index pagination

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,11 @@
 class NotificationsController < ApplicationController
+  def index
+    @unread_notifications = current_user.notifications.unread
+                                        .page(params[:unread_page]).per(3)
+    @read_notifications = current_user.notifications.read
+                                      .page(params[:read_page]).per(3)
+  end
+
   def link_through
     @notification = Notification.find(params[:id])
     @notification.update read: true
